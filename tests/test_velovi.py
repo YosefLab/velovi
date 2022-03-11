ba079c04dd37009d5d2b272d8db321210622d7ea
adamgayoso <adamgayoso@users.noreply.github.com>
2022-03-10 16:34:12 -0800

    name/test fixes

---
+++ tests/test_velovi.py
@@ -0,0 +1,22 @@
+import pyro
+from scvi.data import synthetic_iid
+
+from velovi import VELOVI
+
+
+def test_velovi():
+    n_latent = 5
+    adata = synthetic_iid()
+    adata.layers["spliced"] = adata.X.copy()
+    adata.layers["unspliced"] = adata.X.copy()
+    VELOVI.setup_anndata(adata, unspliced_layer="unspliced", spliced_layer="spliced")
+    model = VELOVI(adata, n_latent=n_latent)
+    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
+    model.get_latent_representation()
+    model.get_velocity()
+    model.get_latent_time()
+    model.get_state_assignment()
+    model.history
+
+    # tests __repr__
+    print(model)
