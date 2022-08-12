43297c14a568f09025d7e921fe962b55845f80e7
adamgayoso <adamgayoso@users.noreply.github.com>
2022-08-12 09:17:32 +0100

    options for preprocess

--- velovi/_utils.py
+++ velovi/_utils.py
@@ -10,6 +10,8 @@ def preprocess_data(
     adata: AnnData,
     spliced_layer: Optional[str] = "Ms",
     unspliced_layer: Optional[str] = "Mu",
+    min_max_scale: bool = True,
+    filter_on_r2: bool = True,
 ) -> AnnData:
     """Preprocess data.
 
@@ -23,22 +25,30 @@ def preprocess_data(
         Name of the spliced layer.
     unspliced_layer
         Name of the unspliced layer.
+    min_max_scale
+        Min-max scale spliced and unspliced
+    filter_on_r2
+        Filter out genes according to linear regression fit
 
     Returns
     -------
     Preprocessed adata.
     """
-    scaler = MinMaxScaler()
-    adata.layers[spliced_layer] = scaler.fit_transform(adata.layers[spliced_layer])
+    if min_max_scale:
+        scaler = MinMaxScaler()
+        adata.layers[spliced_layer] = scaler.fit_transform(adata.layers[spliced_layer])
 
-    scaler = MinMaxScaler()
-    adata.layers[unspliced_layer] = scaler.fit_transform(adata.layers[unspliced_layer])
+        scaler = MinMaxScaler()
+        adata.layers[unspliced_layer] = scaler.fit_transform(
+            adata.layers[unspliced_layer]
+        )
 
-    scv.tl.velocity(adata, mode="deterministic")
+    if filter_on_r2:
+        scv.tl.velocity(adata, mode="deterministic")
 
-    adata = adata[
-        :, np.logical_and(adata.var.velocity_r2 > 0, adata.var.velocity_gamma > 0)
-    ].copy()
-    adata = adata[:, adata.var.velocity_genes].copy()
+        adata = adata[
+            :, np.logical_and(adata.var.velocity_r2 > 0, adata.var.velocity_gamma > 0)
+        ].copy()
+        adata = adata[:, adata.var.velocity_genes].copy()
 
     return adata
